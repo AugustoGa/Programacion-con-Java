8e0a068a31b5b79f981b2e49f68434c5aa9a3ae7
AugustoGa <augustogallo98@gmail.com>
2023-08-20 18:06:37 -0300

    entregable

--- datosFloat/src/main/java/manejoDeDatos.java
+++ datosFloat/src/main/java/manejoDeDatos.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class manejoDeDatos {
+public class manejoDeDatos {
+    public static void main(String[] args) {
+    int numero1 = 1;
+    float numero2 = 2;
+
+        System.out.println(numero1);
+        System.out.println(numero2);
+    }
 }
