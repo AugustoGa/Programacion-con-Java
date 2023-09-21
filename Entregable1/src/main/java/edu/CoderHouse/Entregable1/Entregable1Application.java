87f94bdbf07e346a81c4617731f5b4d7e558d169
AugustoGa <augustogallo98@gmail.com>
2023-09-20 23:04:19 -0300

    Pre Entrega

---
+++ Entregable1/src/main/java/edu/CoderHouse/Entregable1/Entregable1Application.java
@@ -0,0 +1,17 @@
+package edu.CoderHouse.Entregable1;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+import java.util.Optional;
+
+@SpringBootApplication
+public class Entregable1Application  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Entregable1Application.class, args);
+	}
+
+}
---
+++ Entregable1/src/test/java/edu/CoderHouse/Entregable1/Entregable1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.CoderHouse.Entregable1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Entregable1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
